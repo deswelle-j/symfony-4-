ce03a759c73d1c093fd458de5607415cf8c1730c
deswelle-j <deswelle.johan@gmail.com>
2018-04-04 13:47:49 +0200

    create template to display a eventlist and a single event from a array.

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -8,35 +8,37 @@ use Symfony\Component\HttpFoundation\Response;
 
 class EventController extends Controller
 {
-    /**
-     * @Route("/event", name="event")
-     */
+    public function __construct(){
+        return $events = array(
+            array(
+                'id' => '0',
+                'title'   => 'Black cat event',
+                'content' => 'Loremp ipsum ........',
+                'startAt' => new \DateTime('2018-03-12 5:08am'),
+                'shouldEndAt' => new \DateTime('2018-03-12 6:30am'),
+                'place' => '44 rue scrive 59110 La Madeleine',
+            ),
+            array(
+                'id' => '1',
+                'title'   => 'Still alive yet',
+                'content' => 'Lolorem ipsoum .....',
+                'startAt' => new \DateTime('2018-05-21 8:30am'),
+                'shouldEndAt' => new \DateTime('2018-05-21 9:30am'),
+                'place' => '34 de la corniche',
+            ),
+        );
+    }
+    
     public function List(){
         return $this->render('event/list.html.twig', array(
-            'events' => array(
-                'event1' => array(
-                    'title'   => 'Black cat event',
-                    'content' => 'Loremp ipsum ........',
-                    
-                ),
-                'event2' => array(
-                    'title'   => 'Still alive yet',
-                    'content' => 'Lolorem ipsoum .....',
-                ),
-            ),
-        ));
+        'events' => $this->__construct()));
     }
 
     public function show($id){
         $title = 'Nom de l\'evenement';
         $content = 'Description de l\'evenement en question';
         return $this->render('event/show.html.twig', array(
-            'event' => array(
-                'title'   => $title,
-                'content' => $content,
-            ),
-
-        ));
+            'event' => $this->__construct()[$id]));
     }
 
     public function add(){
